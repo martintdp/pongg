7148d038985628506e0175467ea4c8b3fcfce2f0
Martin Toledo Do Pazo <martin.d.toledo@gmail.com>
2016-11-26 18:09:11 -0300

    Adds basic example to test if setup is functional

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,12 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+// Phaser webpack config
+const phaserModule = path.join(__dirname, '/node_modules/phaser/')
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
+const pixi = path.join(phaserModule, 'build/custom/pixi.js')
+const p2 = path.join(phaserModule, 'build/custom/p2.js')
+const box2D = path.join(__dirname, '/src/libs/box2d-plugin-full.js')
+
 module.exports = {
   entry: "./src/js/index.js",
   output: {
@@ -13,8 +20,17 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel' },
-      { test: /\.css$/, loader: "style!css" }
-    ]
+      { test: /\.css$/, loader: "style!css" },
+      { test: /pixi\.js/, loader: 'expose?PIXI' },
+      { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
+      { test: /p2\.js/, loader: 'expose?p2' }   ]
   },
-  plugins: [new HtmlWebpackPlugin()]
-};
+  resolve: {
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'p2': p2,
+    }
+  },
+    plugins: [new HtmlWebpackPlugin()]
+  };
